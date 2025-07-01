5447beaaca860d62070746c656459a2332c7762f
KhanhHoang <khanh.hoangduy@skybull.studio>
2025-07-01 16:34:30 +0700

    up

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    CLIENT_ID: "1376451336266321961", // Discord Application ID
+    COMPANY_NAME: "Skybull", // Company or Developer Name
+    PRODUCT_NAME: "Just Another Generic Roguelike", // Game Name
+    PRODUCT_VERSION: "0.1.0", // Game Version
+};
